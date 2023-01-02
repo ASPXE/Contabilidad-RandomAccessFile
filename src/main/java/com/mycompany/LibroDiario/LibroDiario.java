5bd1c5a8ffc5238f88c5de61dd1c6951baee66f9
ASPXE <aspxe@aspxe-HP-Pavilion-Laptop-15-cw1xxx>
2023-01-02 14:21:03 -0600

    Segundo commit, ya se actualiza el monto del total en el libro mayor dependiendo de los movimientos que haya con un libro diario, solo en el caso de que exista un solo libro diario y un solo libro mayor.
    Para fines del proyecto y por tiempo, funciona lo suficientemente bien 
    como para entregar el proyecto. Tal vez en un futuro lo haga totalmente funcional

---
+++ src/main/java/com/mycompany/LibroDiario/LibroDiario.java
@@ -0,0 +1,162 @@
+package com.mycompany.LibroDiario;
+
+import com.mycompany.ActualizarLibroDiario.AgregarLibroDiario;
+import com.mycompany.MenuPrincipal.MenuPrincipal;
+import java.io.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aspxe
+ */
+public class LibroDiario{
+    
+    public int cuentaNum;
+    public int chequeNum;
+    public String fecha;
+    public String descripcion;
+    public double cargoOAbono;
+    
+    /*
+    LibroMayor y LibroDiario comparten un atributo, el cual es "cuentaNum", pero al ser
+    solo uno he decidido que LibroDiario no será una clase hija de LibroMayor porque los demás
+    atributos no van a ser de ninguna utilidad. Solucionaré el posible error de 
+    que "cuentaNum" pueda no existir en alguna instancia de ambas clases obligando
+    al usuario a que ingrese "cuentaNum". Si que si, educando al usuario XD.
+    */
+    
+    //Constructor vacio
+    public LibroDiario(){
+        
+    }
+    
+    //Constructor con todas las variables
+    public LibroDiario(int cuentaNum, int chequeNum, String fecha, String descripcion, double cargoOAbono){
+        this.cuentaNum = cuentaNum;
+        this.chequeNum = chequeNum;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.cargoOAbono = cargoOAbono;
+    }
+    
+    //Metodo grabar, recibe como parametro el archivo en el que se va a escribir
+    public void grabarLibroDiario(RandomAccessFile file) throws IOException{
+        /**
+         * El orden para grabar el registro es el mismo orden que estan
+         * declarados Registro de longitud fija Campos de longitud fija
+         *
+         */
+        //Escribimos cuentaNum
+        file.writeInt(this.cuentaNum);
+        
+        //Escribimos chequeNum
+        file.writeInt(this.chequeNum);
+        
+        //Escribimos la fecha. Convertimos el string a bytes
+        byte fechaBytes[] = new byte[30]; //30 será la longitud fija
+        this.fecha.getBytes(0, this.fecha.length(), fechaBytes, 0);
+        file.write(fechaBytes);
+        
+        //Escribimos la descripcion. Convertimos el string a bytes
+        byte descripcionBytes[] = new byte[30];
+        this.descripcion.getBytes(0, this.descripcion.length(), descripcionBytes, 0);
+        file.write(descripcionBytes);
+        
+        //Escribimos cargoOAbono
+        file.writeDouble(this.cargoOAbono);
+    }
+    
+    //Metodo leer, recibe como parametro el archivo que se va a leer
+    public void leerLibroDiario(RandomAccessFile file) throws IOException{
+        /*
+        Es importante leer el archivo en el mismo orden que declaramos las variables de la clase
+        */
+        
+        //Leemos cuentaNum
+        this.cuentaNum = file.readInt();
+        
+        //Leemos chequeNum
+        this.chequeNum = file.readInt();
+        
+        //Leemos fecha, reconstruimos el string a partir de los bytes
+        byte fechaByte[] = new byte[30]; //Importante que sea de la misma longitud fija
+        file.readFully(fechaByte);
+        this.fecha = new String(fechaByte);
+        
+        //Leemos descripcion
+        byte descripcionByte[] = new byte[30];
+        file.readFully(descripcionByte);
+        this.descripcion = new String(descripcionByte);
+        
+        //Leemos cargoOAbono
+        this.cargoOAbono = file.readDouble();
+        
+    }
+    
+    //Metodo para validar que existe un libro diario de un año especifico
+    public boolean libroDiarioExiste(String nombreArchivo, AgregarLibroDiario ald){
+        
+        if(nombreArchivo.contains(ald.getYear())){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+   
+    
+    public int getCuentaNum(){
+        return this.cuentaNum;
+    }
+    
+    public void setCuentaNum(int cuentaNum){
+        this.cuentaNum = cuentaNum;
+    }
+
+    public int getChequeNum() {
+        return this.chequeNum;
+    }
+
+    public void setChequeNum(int chequeNum) {
+        this.chequeNum = chequeNum;
+    }
+
+    public String getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getCargoOAbono() {
+        return this.cargoOAbono;
+    }
+
+    public void setCargoOAbono(double cargoOAbono) {
+        this.cargoOAbono = cargoOAbono;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("LibroDiario{");
+        sb.append("cuentaNum=").append(this.cuentaNum);
+        sb.append(", chequeNum=").append(this.chequeNum);
+        sb.append(", fecha=").append(this.fecha);
+        sb.append(", descripcion=").append(this.descripcion);
+        sb.append(", cargoOAbono=").append(this.cargoOAbono);
+        sb.append('}');
+        return sb.toString();
+    }
+
+     
+}
---
+++ src/main/java/com/mycompany/LibroMayor/LibroMayor.java
@@ -0,0 +1,160 @@
+package com.mycompany.LibroMayor;
+
+import com.mycompany.LibroDiario.LibroDiario;
+import com.mycompany.MenuPrincipal.MenuPrincipal;
+import java.io.*;
+
+/**
+ *
+ * @author aspxe
+ */
+public class LibroMayor {
+
+    public int cuentaNum;
+    public String tituloCuenta;
+    public String mes;
+    public double montoMes;
+
+    //Constructor vacio
+    public LibroMayor() {
+
+    }
+
+    //Constructor con todas las variables
+    public LibroMayor(int cuentaNum, String tituloCuenta, String mes, double montoMes) {
+        this.cuentaNum = cuentaNum;
+        this.tituloCuenta = tituloCuenta;
+        this.mes = mes;
+        this.montoMes = montoMes;
+    }
+
+    //Metodo grabar, recibe como parametro el archivo en el que vamos a escribir los datos
+    public void grabarLibroMayor(RandomAccessFile file) throws IOException{
+        /**
+         * El orden para grabar el registro es el mismo orden que estan
+         * declarados Registro de longitud fija Campos de longitud fija
+         *
+         */
+        
+        //Escribimos cuentaNum
+        file.writeInt(this.cuentaNum);
+        
+        //Solo el string lo convertimos a bytes. Escribimos tituloCuenta
+        byte tituloCuentaBytes[] = new byte[30];//La longitud fija sera de 30.
+        this.tituloCuenta.getBytes(0, this.tituloCuenta.length(), tituloCuentaBytes, 0);
+        file.write(tituloCuentaBytes);
+        
+        //Escribimos mes
+        byte mesBytes[] = new byte[30];
+        this.mes.getBytes(0, this.mes.length(), mesBytes, 0);
+        file.write(mesBytes);
+        
+        //Escribimos montoMes
+        file.writeDouble(this.montoMes);
+
+    }
+    
+    //Metodo leer, recibe como parametro el archivo que se va a leer.
+    public void leerLibroMayor(RandomAccessFile file) throws IOException{
+        /*
+        Al igual que en el metodo grabar es importante respetar el orden de como
+        se declararon los atributos de la clase para ser escritos, para este metodo
+        leer es importante leer el contenido en el mismo orden que los atributos fueron
+        escritos.
+        */
+        
+        //Leemos cuentaNum
+        this.cuentaNum = file.readInt();
+        
+        //Reconstruimos el string apartir de los bytes. Leemos tituloCuenta
+        byte tituloCuentaByte[] = new byte[30]; //Importante que el arreglo de bytes sea de la misma longitud que el del metodo grabar.
+        file.readFully(tituloCuentaByte);
+        this.tituloCuenta = new String(tituloCuentaByte);
+        
+        //Reconstruimos el string a partir de los bytes. Leemos mes
+        byte mesByte[] = new byte[30];
+        file.readFully(mesByte);
+        this.mes = new String(mesByte);
+        
+        //Leemos montoMes
+        this.montoMes = file.readDouble();
+        
+    }
+    
+    //Leer libro mayor con puntero
+    public void leerLibroMayor(RandomAccessFile file, LibroMayor lm) throws IOException{
+        /*
+        Al igual que en el metodo grabar es importante respetar el orden de como
+        se declararon los atributos de la clase para ser escritos, para este metodo
+        leer es importante leer el contenido en el mismo orden que los atributos fueron
+        escritos.
+        */
+        
+        //Leemos cuentaNum
+        this.cuentaNum = file.readInt();
+        
+        //Reconstruimos el string apartir de los bytes. Leemos tituloCuenta
+        byte tituloCuentaByte[] = new byte[30]; //Importante que el arreglo de bytes sea de la misma longitud que el del metodo grabar.
+        file.readFully(tituloCuentaByte);
+        this.tituloCuenta = new String(tituloCuentaByte); 
+        
+        //Reconstruimos el string a partir de los bytes. Leemos mes
+        byte mesByte[] = new byte[30];
+        file.readFully(mesByte);
+        this.mes = new String(mesByte);      
+        //Leemos montoMes
+        this.montoMes = file.readDouble(); 
+        
+        lm.setCuentaNum(this.cuentaNum);
+        lm.setTituloCuenta(this.tituloCuenta);
+        lm.setMes(this.mes);
+        lm.setMontoMes(this.montoMes);
+        
+    }
+
+    //Metodos get y set de las variables de la clase
+    public int getCuentaNum() {
+        return this.cuentaNum;
+    }
+
+    public void setCuentaNum(int cuentaNum) {
+        this.cuentaNum = cuentaNum;
+    }
+
+    public String getTituloCuenta() {
+        return this.tituloCuenta;
+    }
+
+    public void setTituloCuenta(String tituloCuenta) {
+        this.tituloCuenta = tituloCuenta;
+    }
+
+    public String getMes() {
+        return this.mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public double getMontoMes() {
+        return this.montoMes;
+    }
+
+    public void setMontoMes(double montoMes) {
+        this.montoMes = montoMes;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("LibroMayor{");
+        sb.append("cuentaNum=").append(this.cuentaNum);
+        sb.append(", tituloCuenta=").append(this.tituloCuenta);
+        sb.append(", mes=").append(this.mes);
+        sb.append(", montoMes=").append(this.montoMes);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
